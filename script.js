5003c84d2bc9e952ea73d35b92a44bc57941077a
Renato Kato <128105293+Kato1010@users.noreply.github.com>
2023-09-27 00:08:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// módulo do JavaScript para melhor organização
+//Parte do menu
+const menu = document.querySelector('.menu');
+const NavMenu = document.querySelector('.nav-menu');
+menu.addEventListener('click', () => {
+    menu.classList.toggle('ativo');
+    NavMenu.classList.toggle('ativo');
+})
+
+
+
+
+
+
+
+
+
+
+
